aed066ff356c8c207c754cc0cd33e6ef97160ab8
kvrieze <139392367+kvrieze@users.noreply.github.com>
2023-07-12 18:15:33 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <style>
-    .button {
-      display: inline-block;
-      padding: 10px 20px;
-      background-color: #4CAF50;
-      color: white;
-      text-decoration: none;
-      border: none;
-      cursor: pointer;
-      font-size: 16px;
-      border-radius: 4px;
-    }
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
-    .button:hover {
-      background-color: #45a049;
-    }
-  </style>
-</head>
-<body>
-  <h1>Welcome to My Website</h1>
-  <p>Click the button below to go to another page:</p>
-  <a href="page2.html" class="button">Go to Page 2</a>
-</body>
-</html>
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
+      <main>
+        <Header title="Welcome to Group Travel Odyssey's New Test Page!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
 }
