364e20ea278ea9796d93e61b250d852c1c103f1f
kvrieze <139392367+kvrieze@users.noreply.github.com>
2023-07-12 18:23:48 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to Group Travel Odyssey's New Test Page!" />
+        <Header title="Welcome to Group Travel Odyssey's New Page!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
