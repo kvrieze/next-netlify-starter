416dfa25602b2ef5fa36236cad250d9d5b9c37fa
kvrieze <139392367+kvrieze@users.noreply.github.com>
2023-07-12 18:19:07 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="https://grouptravelodyssey.quickbase.com/up/bq2zenuiz/a/r2/e19/v0" alt="GTO Logo" className={styles.logo} />
+        <img src="public/GTOLetterLogo.png" alt="GTO Logo" className={styles.logo} />
       </footer>
     </>
   )
